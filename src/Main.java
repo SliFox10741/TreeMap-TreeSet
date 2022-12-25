eef7119e2ff0b6cc25079d776c67e8c714fa1f9b
Maxim <mr.maximgus@gmail.com>
2022-12-25 07:23:09 +0500

    Fix 2 (Выполнение доп задания)

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         List<Person> peoples = new ArrayList<>();
-        peoples.add(new Person("Maxim","Guselnikov Petrov King",8));
-        peoples.add(new Person("Roman","Pupkin",14));
-        peoples.add(new Person("Boris","Diadkin Vinissuella",64));
-        peoples.add(new Person("katya","Franken",32));
-        peoples.add(new Person("Vasya","Asgin",90));
+        peoples.add(new Person("Maxim","Guselnikov Petrov King Eight Konovalov",8));
+        peoples.add(new Person("Roman","Pupkin ",14));
+        peoples.add(new Person("Boris","Diadkin",64));
+        peoples.add(new Person("katya","Franken Vinissuella",32));
+        peoples.add(new Person("Vasya","Asgin Berlin Moscow Kusnecov",90));
 
 
         SurnameComparator surnameComparator = new SurnameComparator();
